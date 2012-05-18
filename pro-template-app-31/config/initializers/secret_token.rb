e983cd758e314344d3d19d4e919d66ec6c22c83c
Viktor <viktor.markevich@faceit.com.ua>
2012-05-18 17:42:16 +0300

    initial commit

---
+++ pro-template-app-31/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProTemplateApp31::Application.config.secret_token = '150af4d5bd55a9457e1b02b373a095e72ee14ecf5597babd51cd2a1bddca12346b527c3cc71fdd119c26faa4fa473f928d4b9ffb17f6f7c89caf5995c37164dd'
