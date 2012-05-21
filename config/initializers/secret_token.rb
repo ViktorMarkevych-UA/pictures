dad01cac9c8b5b29122b9602a54fcf6bc05c87a6
Viktor <viktor.markevich@faceit.com.ua>
2012-05-21 09:23:53 +0300

    Analitics
    Заливка файлов

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Analitics::Application.config.secret_token = 'dc38685b18ea30f1773571ec16096433f0b5e95c0fa46ef3fd84c2ebbee92cf9f3ea99c55331d65bb993dcf03cc90c3604909ee458ca28b73982430b3ea2150f'
