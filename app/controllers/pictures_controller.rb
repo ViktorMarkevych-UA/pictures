d85e6bee73af39b7d6599460a1fc3d77ae00427b
Viktor <viktor.markevich@faceit.com.ua>
2012-07-30 14:22:58 +0300

    add
    add

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,24 @@
+class PicturesController < ApplicationController
+
+  def index
+    @picture = Pictures.all
+  end
+
+  def all_img
+    @pictures = Picture.order("likes_count DESC")
+  end
+
+  def show
+    @picture = Picture.includes(:comments).find(params[:id])
+  end
+
+  def like
+    @picture = Picture.find(params[:id])
+    unless @picture.likes.find_by_user_id(current_user.id)
+      like = @picture.likes.build(:user_id => current_user.id)
+      flash[:error] = "Error" unless like.save
+    end
+    redirect_to :back
+  end
+
+end
