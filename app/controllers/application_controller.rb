e7f7b64af014271ec81eaa0c235e17d10072b89d
Victor <viktor.markevich@faceit.com.ua>
2017-10-04 12:23:46 +0300

    Change links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :authenticate_user!
-  before_filter :event_links
+  before_action :authenticate_user!
+  before_action :event_links
 
   def event_links
     Event.create(:eventtable_type => "Navigation", :user_id => current_user.id, :eventtable_body => request.env["REQUEST_URI"] ) if current_user
