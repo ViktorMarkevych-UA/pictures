e638e42e84f6123b24be3547fd513d35f5a602a7
Victor <viktor.markevich@faceit.com.ua>
2017-10-19 08:57:35 +0300

    Change syntax

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,12 +2,10 @@ class Picture < ActiveRecord::Base
   belongs_to :category
   has_many :comments
   has_many :likes
-  has_many :users, :through => :likes
+  has_many :users, through: :likes
+  
+  validates :category_id, presence: true
 
-  # attr_accessible :category_id, :pict
-
-  validates :category_id, :presence => true
-
-  has_attached_file :pict, :styles => { :large => "640x480", :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :pict, styles: { large: '640x480', medium: '242x200#', thumb: '171x180>' }
   validates_attachment_content_type :pict, content_type: /\Aimage\/.*\Z/
 end
