d0b95e3af3e8fe961f645199c820c5c2cecaba11
Victor <viktor.markevich@faceit.com.ua>
2017-12-12 17:22:56 +0200

    Customize

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,18 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :event_links
 
+  layout :select_layout
+
   def event_links
     if user_signed_in?
       current_user.events.create(eventtable_type: 'Navigation',
                                  eventtable_body: request.env['REQUEST_URI'] )
     end
+  end
+
+  protected
+
+  def select_layout
+    devise_controller? ? 'authorization' : 'application'
   end
 end
