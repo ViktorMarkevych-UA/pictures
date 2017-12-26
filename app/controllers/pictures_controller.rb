a296a894e13def2fa7a203379421e13e4e44c223
Victor <viktor.markevich@faceit.com.ua>
2017-12-26 20:18:46 +0200

    Display photo

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,21 +1,13 @@
 class PicturesController < ApplicationController
 
   def index
-    pictures =
+    @pictures =
         if params[:category_id]
-          Picture.where(:category_id => params[:category_id]).order("likes_count DESC")
+          Picture.where(:category_id => params[:category_id]).order("likes_count ASC")
         else
-          Picture.order("likes_count DESC").all
+          Picture.order("likes_count ASC").all
         end
-
-    # @pictures = pictures.collect{ |pic| [pic.image.url(:large).html_safe, pic.id, Rails.root.to_s + 'public/' + Paperclip::Geometry.from_file(pic.image.url(:large).split('?')).first.width.to_i] }
-    @pictures = pictures.each_with_object([]) do |picture, result|
-      result << [picture.image.url(:large).html_safe, picture.id]
-      p '*'*100
-      p picture.image.url(:large)
-      # p Paperclip::Geometry.from_file(Rails.root.to_s + '/public' + picture.image.url(:original).split('?')[0])
-      p '*'*100
-    end
+    @pictures = @pictures.map{ |picture| [picture.image.url(:large).html_safe, picture.id] }
   end
 
   def show
